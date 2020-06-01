3c64694aedfcc75b4c515054ad2e0ebc6627a6f8
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-06-01 12:08:20 -0600

    Updates

---
+++ precache-manifest.44458c2760205bb8a2a74f2a5c99b7c8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc4ddb4dbb697fd8522",
+    "url": "/Clicky-Game/static/css/main.3b26611e.chunk.css"
+  },
+  {
+    "revision": "fdc4ddb4dbb697fd8522",
+    "url": "/Clicky-Game/static/js/main.c0489eb5.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "7be1b8c74ace3eb0afb1",
+    "url": "/Clicky-Game/static/js/2.70e67cba.chunk.js"
+  },
+  {
+    "revision": "9c9bd2026903a115f032199cb4af0386",
+    "url": "/Clicky-Game/static/media/two.9c9bd202.png"
+  },
+  {
+    "revision": "ec14be77821eb164c02a1866735f8208",
+    "url": "/Clicky-Game/static/media/three.ec14be77.png"
+  },
+  {
+    "revision": "a7f3b99ad38c6adaab7c5827b33c87f7",
+    "url": "/Clicky-Game/static/media/four.a7f3b99a.png"
+  },
+  {
+    "revision": "668bf7c4ad34f60bc0893311c635eb3c",
+    "url": "/Clicky-Game/static/media/five.668bf7c4.png"
+  },
+  {
+    "revision": "175f2eda4d1fc60fd7ade6ffe50c4379",
+    "url": "/Clicky-Game/static/media/six.175f2eda.png"
+  },
+  {
+    "revision": "b889af1450ae319e5c96938d090973fe",
+    "url": "/Clicky-Game/static/media/seven.b889af14.png"
+  },
+  {
+    "revision": "ebbf50ba6086af36dd97a4d14749b0e7",
+    "url": "/Clicky-Game/static/media/eight.ebbf50ba.png"
+  },
+  {
+    "revision": "99bb12928fe5b9bd9dfec0ced89fe4c4",
+    "url": "/Clicky-Game/static/media/nine.99bb1292.jpg"
+  },
+  {
+    "revision": "161934493b3de29e3a4b3aaf4cd26875",
+    "url": "/Clicky-Game/static/media/ten.16193449.jpg"
+  },
+  {
+    "revision": "68cb2ef8296c994e963e6a6177c8992a",
+    "url": "/Clicky-Game/static/media/eleven.68cb2ef8.jpg"
+  },
+  {
+    "revision": "838f5533a701533431a95937896556c0",
+    "url": "/Clicky-Game/static/media/tweleve.838f5533.jpg"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/Clicky-Game/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "d2562d832f035514acb500eabfb4858d",
+    "url": "/Clicky-Game/index.html"
+  }
+];
